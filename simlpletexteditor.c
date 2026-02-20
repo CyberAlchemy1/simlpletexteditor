8ef9fbc4f2ee4c2389fa6308d350cb580aad4748
CyberAlchemy1 <nyakundilinton@gmail.com>
2026-02-20 22:20:21 +0300

    added a way to input

--- simlpletexteditor.c
+++ simlpletexteditor.c
@@ -23,7 +23,7 @@ void load_file(const char *filename){
 void save_file(const char *filename){
     FILE *file = fopen(filename,"w");
     if(file ==NULL){
-        perror("Error opening file");
+        perror("Error opening files");
         return;
     }
     for(int i =0; i<line_count; i++){
@@ -37,6 +37,17 @@ void print_buffer(){
         printf("%s", buffer[i]);
     }
 }
+void insert_text(){
+    printf("Enter text (end with an empty line): \n");
+    char input[MAX_LINE_LENGTH];
+    while (fgets(input,MAX_LINE_LENGTH,stdin)){
+        if (input[0]=='\n'){
+            break;
+        }
+        strcpy(buffer[line_count],input);
+        line_count++;
+    }
+}
 int main(int argc,char *argv[]){
     if(argc !=2){
         printf("Usage: %s <filename>\n", argv[0]);
@@ -44,7 +55,7 @@ int main(int argc,char *argv[]){
     }
     load_file(argv[1]);
     print_buffer();
-    printf("\nEditing functionality not implemented yet.\n");
+    insert_text();
     save_file(argv[1]);
     return 0;
 }
